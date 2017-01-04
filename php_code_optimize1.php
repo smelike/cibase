36b5752f291f23a89c06d5830ee781336e281fd3
smelike <xxj8711@gmail.com>
2017-01-04 09:59:51 +0800

    Optimize
    1. new a variable and assign the values;
    2. the variables have a same intialize value, keep in the same line;
    3. keep one return

--- php_code_optimize1.php
+++ php_code_optimize1.php
@@ -1,6 +1,6 @@
 <?php
 
-// 获取汇率
+	// Before optimize 获取汇率
     protected function get_rate($platform_id){
         $rate_table = 'company_rate a';
         $rate_join = array();
@@ -24,4 +24,29 @@
     }
 
 	
-	
+	// After optimize 获取汇率
+    protected function get_rate($platform_id) {
+        $rate_table = 'company_rate a';
+        $rate_join = array(
+            array('currency b','b.id = a.currency_id','left'),
+            array('platform c','c.currency_id = b.id','left')
+        );
+
+        $rate_wh = array(
+            'c.id' => intval($platform_id),
+            'a.company_id' => $this->company_id,
+            'a.status' => 1
+        );
+        $rate_fields = 'a.id,a.real_rate,b.name,b.rate,b.code';
+        $rate_order = $rate_group = '';
+        $rate_first = $rate_num = 0;
+        $rate_info = $this->Data_model->getJoinData($rate_table, $rate_join, $rate_wh, $rate_fields, $rate_order, $rate_group, $rate_first, $rate_num);
+
+        $rate = 0;
+        // 当前货币汇率 + 某个公司的浮动汇率
+        if($rate_info) {
+            $rate = floatval($rate_info[0]['rate']) + floatval($rate_info[0]['real_rate']);
+        }
+
+        return $rate;
+    }
